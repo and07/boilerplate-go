25fa76013642aa361c4efb7d7ed0e625fb2fd420
shine_86 <nundalov86@gmail.com>
2020-10-26 10:22:35 +0300

    command line args

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/temp-lib/templater"
 )
 
 func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("usage : app serviceName user")
+		return
+	}
 	cfg := templater.Config{
-		ServiceName:    "TEST",
-		User:           "shine",
+		ServiceName:    os.Args[1],
+		User:           os.Args[2],
 		IgnorePatterns: []string{".html", ".yml", ".yaml", ".tpl", ".txt", "js.map"},
 	}
 	templtr := templater.New(cfg)
