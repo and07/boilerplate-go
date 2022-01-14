03f5b182ca17c3b4db5692bb7b5175907cb5fb87
and07 <41157447+and07@users.noreply.github.com>
2022-01-14 16:33:16 +0300

    Update handle.go

--- cmd/boilerplate-go/handle.go
+++ cmd/boilerplate-go/handle.go
@@ -7,14 +7,21 @@ import (
 	"github.com/and07/boilerplate-go/internal/pkg/template"
 )
 
+
+
+
 func hiHandler(ctx context.Context, tpl *template.Template) func(w http.ResponseWriter, r *http.Request) {
 
 	return func(w http.ResponseWriter, r *http.Request) {
 		//span, _ := opentracing.StartSpanFromContext(ctx, "Scratch.hiHandler")
 		//defer span.Finish()
 		counter.Inc()
-
-		tpl.RenderTemplate(w, "main.html", "Hi")
+		
+		xRealIP := r.Header.Get("X-Real-Ip")
+		xForwardedFor = r.Header.Get("X-Forwarded-For")
+		remoteAddr = r.RemoteAddr
+		
+		tpl.RenderTemplate(w, "main.html", fmt.Sprintf("X-Real-Ip:%s X-Forwarded-For:%s RemoteAddr:%s", xRealIP, xForwardedFor, remoteAddr))
 	}
 }
 
